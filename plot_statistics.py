dedc5edbe24adbfaae1cb5ee5d28a49251571d4a
mcpca <mca@netc.eu>
2021-09-10 19:10:07 +0200

    Update plotting script

--- plot_statistics.py
+++ plot_statistics.py
@@ -6,13 +6,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn
 import pandas
+import scipy.stats as stats
 
 
 def parse_args():
     ap = ArgumentParser()
 
-    ap.add_argument("godunov_data", type=str)
-    ap.add_argument("godunov_np_data", type=str)
+    ap.add_argument("greenshields_data", type=str)
+    ap.add_argument("greenshields_np_data", type=str)
+    ap.add_argument("newelldaganzo_data", type=str)
+    ap.add_argument("newelldaganzo_np_data", type=str)
     ap.add_argument("sumo_data", type=str)
     ap.add_argument("sumo_np_data", type=str)
 
@@ -36,36 +39,52 @@ def get_data(path):
 def main():
     args = parse_args()
 
-    godunov = get_data(args.godunov_data)
-    godunov_np = get_data(args.godunov_np_data)
+    greenshields = get_data(args.greenshields_data)
+    greenshields_np = get_data(args.greenshields_np_data)
+    newelldaganzo = get_data(args.newelldaganzo_data)
+    newelldaganzo_np = get_data(args.newelldaganzo_np_data)
     sumo = get_data(args.sumo_data)
     sumo_np = get_data(args.sumo_np_data)
 
-    colnames = ['Godunov', 'Godunov (NP)', 'SUMO', 'SUMO (NP)']
+#     colnames = [
+#         'Greenshields', 'Greenshields (NP)', 'ND', 'ND (NP)', 'SUMO',
+#         'SUMO (NP)']
 
-    error = pandas.DataFrame(
-            np.array([
-                godunov['rmse'], godunov_np['rmse'], sumo['rmse'],
-                sumo_np['rmse']
-                ]).T,
-            columns=colnames)
+    colnames = ['Error', 'Time', 'Case', 'Pretraining']
 
-    time = pandas.DataFrame(
-            np.array([
-                godunov['time'], godunov_np['time'], sumo['time'],
-                sumo_np['time']
-                ]).T,
-            columns=colnames)
+    data = [ (error, time, 'Greenshields', 'Yes') for error, time in
+                zip(greenshields['rmse'], greenshields['time'])
+            ] + [ (error, time, 'Greenshields', 'No') for error, time in
+                zip(greenshields_np['rmse'], greenshields_np['time'])
+            ] + [ (error, time, 'Newell-Daganzo', 'Yes') for error, time in
+                zip(newelldaganzo['rmse'], newelldaganzo['time'])
+            ] + [ (error, time, 'Newell-Daganzo', 'No') for error, time in
+                zip(newelldaganzo_np['rmse'], newelldaganzo_np['time'])
+            ] + [ (error, time, 'SUMO', 'Yes') for error, time in
+                zip(sumo['rmse'], sumo['time'])
+            ] + [ (error, time, 'SUMO', 'No') for error, time in
+                zip(sumo_np['rmse'], sumo_np['time']) ]
 
-    for var, label, axlabel, title in zip(
-            (time, error), ('time', 'rmse'), ('Time [sec]', 'RMSE'),
-            ('Computation time', 'Density error')):
-        fig, ax = plt.subplots()
-        seaborn.boxplot(data=var, ax=ax)
-        ax.grid()
-        ax.set_ylabel(axlabel)
-        ax.set_title(title)
-        fig.savefig(label + '.eps', dpi=200)
+    alldata = pandas.DataFrame(data, columns=colnames)
+
+    # Remove outliers in simulation time
+    score = np.abs(stats.zscore(alldata['Time']))
+    alldata = alldata[(score < 4)]
+
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    seaborn.boxplot(data=alldata, x='Case', y='Time', hue='Pretraining',
+            ax=ax[0])
+    seaborn.boxplot(data=alldata, x='Case', y='Error', hue='Pretraining',
+            ax=ax[1])
+
+    ax[0].set_ylabel('Time [sec]')
+    ax[1].set_ylabel('Generalization Error')
+    ax[0].set_yscale('log')
+    ax[1].set_yscale('log')
+    ax[1].get_legend().remove()
+    ax[0].set_xlabel('')
+    ax[1].set_xlabel('')
+    plt.show()
 
 
 if __name__ == "__main__":
